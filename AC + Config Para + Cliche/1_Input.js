af1cd81382dc5e4bb36c73791a3a478bb6d78c97
clauds-clauds <217328249+clauds-clauds@users.noreply.github.com>
2025-09-10 17:18:07 +0200

    add auto-cards

---
+++ AC + Config Para + Cliche/1_Input.js
@@ -0,0 +1,8 @@
+// Your "Input" tab should look like this
+const modifier = (text) => {
+    // Your other input modifier scripts go here (preferred)
+    text = AutoCards("input", text);
+    // Your other input modifier scripts go here (alternative)
+    return { text };
+};
+modifier(text);
